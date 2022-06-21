1b60952ec9cba0cdb3836616f63ce4bc21cb2880
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-21 18:42:43 -0300

    feat: created main entrypoint api

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// const errorHandler = require('./middlewares/errorHandler');
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+
+//app.use(errorHandler);
+
+app.listen(port, () => console.log(`Servidor online na porta ${port}`));
