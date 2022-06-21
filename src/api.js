cec9a4961abf6ae132717114b4c34fca3f2c99c4
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-21 19:27:41 -0300

    feat: created errorHandler Middleware

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const errorHandler = require('./middlewares/errorHandler');
+const errorHandler = require('./middlewares/errorHandler');
 
 
 const app = express();
@@ -14,7 +14,6 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-
-//app.use(errorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Servidor online na porta ${port}`));
