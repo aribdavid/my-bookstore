3f5b788e8b0649bd75943637f3bdf64feda0f631
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-22 12:54:37 -0300

    feat:created author router

---
+++ src/router/authorRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const rescue = require('express-rescue');
+const authorController = require('../controller/authorController');
+
+const tokenAuthenticator = require('../middleware/tokenAuthenticator');
+
+const router = express.Router();
+
+
+router.get('/author',rescue(tokenAuthenticator), rescue(authorController.getAll));
+// router.get('/author/:id', rescue(tokenAuthenticator), rescue(authorController.getById));
+
+
+module.exports = router;
